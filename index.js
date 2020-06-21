5043a2b570bca34d91917131e9c57bd384df30f7
Harry Kim <tlonist.harry@gmail.com>
2020-06-21 13:33:20 +0900

    'initial-commit'

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express'); //commonjs module
+// import express from 'express' => es2016 module
+
+const app = express();
+app.get('/', (req, res) => {
+    res.send({hi:'there'});
+});
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
