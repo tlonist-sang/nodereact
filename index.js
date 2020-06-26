ca4029e5ccfdb70b883fcf2531718ff4f0c9e543
Harry Kim <tlonist.harry@gmail.com>
2020-06-26 20:18:12 +0900

    'finishedAuthFlow'

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express'); //commonjs module
-// import express from 'express' => es2016 module
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
+require('./models/User');
+require('./services/passport'); // import express from 'express' => es2016 module
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
-app.get('/', (req, res) => {
-    res.send({hi:'bye~'});
-});
+app.use(
+    cookieSession({
+        maxAge: 30*24*60*60*1000,
+        keys: [keys.cookieKey]
+    })
+);
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
