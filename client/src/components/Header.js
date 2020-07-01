c85db1ba5ec36618c0e23f1425057dd19514e4e9
Harry Kim <tlonist.harry@gmail.com>
2020-07-02 08:36:24 +0900

    'front'

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,17 +1,35 @@
 import React from "react";
+import {useSelector} from "react-redux";
+import {Link} from "react-router-dom";
 
 const Header = () => {
+    const auth = useSelector(state => state.auth);
+
+    const renderContent = () => {
+      switch(auth){
+          case null:
+              return;
+          case false:
+              return (
+                  <li><a href={"/auth/google"}>Login With Google</a></li>
+              );
+          default:
+              return <li><a href={"/api/logout"}>Logout</a></li>;
+      }
+    };
+
     return (
         <nav>
             <div>
                 <div className={"nav-wrapper"}>
-                    <a className={"left brand-logo"}>
+                    <Link
+                        to={auth? '/surveys': '/'}
+                        className={"left brand-logo"}
+                    >
                         Emaily
-                    </a>
+                    </Link>
                     <ul className={"right"}>
-                        <li>
-                            <a>Login With Google</a>
-                        </li>
+                        {renderContent()}
                     </ul>
                 </div>
             </div>
