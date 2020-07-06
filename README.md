21046cba1a15bc18ecb2a2375bb955d9301a651c
tlonist.sang <tlonist.sang@gmail.com>
2020-07-06 11:50:11 +0900

    mailing

---
+++ README.md
@@ -0,0 +1,27 @@
+Covered here
+
+Frontend
+- Re-routing with proxy middleware
+- React + Redux
+- Reduxthunk
+- materialize - css
+
+Backend
+- MongoDB with Mongoose (data modelling)
+- Applying custom middlewares for requests
+- Switching of Dev & Prod environment
+- Oauth2 (Google)
+- Authentication with Passport.js
+- SendGrid API (email)
+- Stripe API (payment)
+
+Database
+- MongoDB cloud
+
+CI/CD
+- Heroku
+- Git/ Github
+
+Others libraries used
+- nodemon
+- concurrently for booting both frontend and backend at the same time
---
+++ client/build/precache-manifest.ff27b9800857f2130bfc21f5e5651ba0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "422d835c512fc850f71cd2c547b62c1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c34a761889f61c3111d",
+    "url": "/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "3c34a761889f61c3111d",
+    "url": "/static/js/2.0eb691db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0eb691db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5f2f15656c1e868ed16",
+    "url": "/static/js/main.c6c356cd.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
